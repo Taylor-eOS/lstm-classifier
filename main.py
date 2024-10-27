Corrected formula

main.py
@@ -19,12 +19,12 @@ HOP_LENGTH = 256
 HIDDEN_SIZE = 128
 NUM_LAYERS = 2
 BATCH_SIZE = 32
-LEARNING_RATE = 0.00006
+LEARNING_RATE = 0.00008
 EARLY_STOPPING = False
 ACCURACY_THRESHOLD = 0.992
 MIN_ACCURACY = 0.65
 BATCHES_PER_EPOCH = 32
-MAX_EPOCHS = 40
+MAX_EPOCHS = 50
 
 class AudioClassifier(nn.Module):
     def __init__(self):
@@ -130,21 +130,21 @@ def main(mode, batch_size=BATCH_SIZE, input_file=None, model=None):
         total_batches = len(train_dataset) / BATCH_SIZE
         print(f'There are {len(train_dataset)} samples, split up into {total_batches} batches.')
         if not total_batches.is_integer():
-            print(f'Data file amount not divisible by {BATCHES_PER_EPOCH} batches per epoch.')
+            print(f'Sample size not divisible by {BATCHES_PER_EPOCH} batches per epoch.')
         print(f'Batch sampling: {BATCHES_PER_EPOCH} batches per epoch')
         print(f'Learning rate: {LEARNING_RATE:.10f}'.rstrip('0').rstrip('.'))
         print(f'Sampling rate: {SAMPLING_RATE}')
         print(f'Number of MFCC spectra: {N_MFCC}')
         print(f'Sequence length: {SEQ_LENGTH}')
         print(f'Hidden size: {HIDDEN_SIZE}')
-        print('-Training:')
+        print('-Training:-')
         last_loss = 1.0
         highest_accuracy = MIN_ACCURACY
         best_model = None
         for epoch in range(max_epochs):
             print(f'Epoch {epoch+1}/{max_epochs}')
-            unique_coverage = (total_batches * (1 - ((total_batches - BATCHES_PER_EPOCH) / total_batches) ** epoch)) / total_batches
-            print(f'Unique coverage: {unique_coverage:.4f}')
+            unique_coverage = (1 - ((total_batches - BATCHES_PER_EPOCH) / total_batches) ** epoch) * 100
+            print(f'Unique coverage: {unique_coverage:.2f}%')
             start_time = time.time()
             train(model, train_dataset, criterion, optimizer)
             elapsed_time = time.time() - start_time